91ff83c81c9091d1bbce13d920b7f777160b1019
yyyenaak <dpsk185@gmail.com>
2025-02-20 15:47:38 +0900

    useState 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집"; //변수 선언
+  let [a, b] = useState("남자 코드 추천");
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{ fontSize: "16px" }}>블로그</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{a}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
