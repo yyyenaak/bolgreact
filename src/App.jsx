c1e5b56b2c707b0d25f9e8ede0a413b1c23f4fde
yyyenaak <dpsk185@gmail.com>
2025-02-20 17:46:29 +0900

    modal UI 동작 완성성

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   ]); //재랜더링시 useState를 사용
 
   let [좋아요, 좋아요변경] = useState(0); //state 변경함수, 좋아요 변경 넣을때 +1 늘어나게
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -46,25 +47,28 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {글제목[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <Modal></Modal>
-      <Modal />
+      {modal === true ? <Modal></Modal> : null}
     </div>
   );
 }
 
 function Modal() {
   return (
-    <>
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-    </>
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   );
 }
 
