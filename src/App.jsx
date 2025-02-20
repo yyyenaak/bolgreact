3633a13f6c3d021c4c426c90507d09e6749faa68
yyyenaak <dpsk185@gmail.com>
2025-02-20 16:26:12 +0900

    제목변경 버튼 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집"; //변수 선언
-  let [글제목, b] = useState([
+  let [글제목, 글제목변경] = useState([
     "남자 코드 추천",
     "강남 우동맛집",
     "파이썬 독학",
@@ -17,6 +17,16 @@ function App() {
       <div className="black-nav">
         <h4>Blog-React</h4>
       </div>
+
+      <button
+        onClick={() => {
+          let copy = 글제목;
+          copy[0] = "여자코드 추천";
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
       <div className="list">
         <h4>
           {글제목[0]}
