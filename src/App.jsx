0c854c0c930c0dd0fb6c1795bd9932a302a0ea1e
yyyenaak <dpsk185@gmail.com>
2025-02-20 16:00:42 +0900

    usestate array 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집"; //변수 선언
-  let [a, b] = useState("남자 코드 추천");
+  let [글제목, b] = useState([
+    "남자 코드 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]); //재랜더링시 useState를 사용
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ fontSize: "16px" }}>블로그</h4>
+        <h4>Blog-React</h4>
       </div>
       <div className="list">
-        <h4>{a}</h4>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
