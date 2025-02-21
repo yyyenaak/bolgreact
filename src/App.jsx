1f373a5d29e98ce8fa0d72f2870107c72a32c302
yyyenaak <dpsk185@gmail.com>
2025-02-21 18:05:25 +0900

    props 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */ //lint 에러 무시
 import { useState } from "react";
@@ -9,13 +10,12 @@ function App() {
     "남자 코드 추천",
     "강남 우동맛집",
     "파이썬 독학",
-    87,
   ]); //재랜더링시 useState를 사용
 
   let [좋아요, 좋아요변경] = useState(0); //state 변경함수, 좋아요 변경 넣을때 +1 늘어나게
   let [modal, setModal] = useState(false);
 
-  [1, 2].map(function (a) {
+  [1, 2, 3].map(function (a) {
     console.log(a);
   });
 
@@ -34,7 +34,7 @@ function App() {
       >
         글수정
       </button>
-      <div className="list">
+      {/* <div className="list">
         <h4>
           {글제목[0]}
           <span
@@ -61,13 +61,17 @@ function App() {
           {글제목[2]}
         </h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
 
-      {[1, 2].map(function () {
+      {글제목.map(function (a, i) {
         return (
           <div className="list">
-            <h4>
-              {글제목[0]}
+            <h4
+              onClick={() => {
+                setModal(!modal); //! 은 modal의 현재 행동을 바꿔주는것 "true" -> "false", "false" -> "true"
+              }}
+            >
+              {글제목[i]}
               <span
                 onClick={() => {
                   좋아요변경(좋아요 + 1);
@@ -82,15 +86,15 @@ function App() {
         );
       })}
 
-      {modal === true ? <Modal></Modal> : null}
+      {modal === true ? <Modal color="skyblue" 글제목={글제목}></Modal> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{ background: props.color }}>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
